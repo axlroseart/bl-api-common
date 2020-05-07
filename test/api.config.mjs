c48fe8e604206a4ca7564f598f3cd43b5a894c25
Ryan Xue <axlroseart@163.com>
2020-05-07 14:16:30 +0800

    update

--- test/api.config.mjs
+++ test/api.config.mjs
@@ -22,55 +22,6 @@ const apiConfig = {
     type: 'url',
     params: {}
   },
-  // 首页banner图
-  homeBanner: {
-    url: '/v1/product-center/getBanner',
-    method: 'GET',
-    type: 'url',
-    params: {}
-  },
-  // 客户列表
-  clientInterface: {
-    url: '/v1/membership/customer/customers',
-    method: 'GET',
-    type: 'url',
-    params: {}
-  },
-  // 客户详情
-  clientDescribe: {
-    url: '/v1/membership/customer/customer',
-    method: 'GET',
-    type: 'url',
-    params: {}
-  },
-  // 添加客户、修改用户信息
-  addClient: {
-    url: '/v1/membership/customer/customer',
-    method: 'POST',
-    type: 'mix',
-    params: {},
-    extra: {}
-  },
-  // 删除客户
-  deleteClient: {
-    url: '/v1/membership/customer/delete_customer',
-    method: 'POST',
-    type: 'mix',
-    params: {},
-    extra: {}
-  },
-  // 保险公司下的 - 所有险种信息
-  fetchAllInsureList: {
-    url: '/api/api/invoke',
-    method: 'POST',
-    type: 'mix',
-    params: {
-      view: 'true',
-      server: 'PolicyIns.getProductList',
-      device: 'mobile'
-    },
-    extra: {}
-  },
   //验证码登录
   verifyLogin: {
     url: '/v1/sms-verify',
@@ -78,86 +29,18 @@ const apiConfig = {
     type: 'route',
     params: {}
   },
-
-  // 产品库接口
-  fetchCurrProductData: {
-    url: '/v1/product-center/products',
-    method: 'GET',
-    type: 'url',
-    params: {}
-  },
-  // 获取token
-  fetchToken: {
-    url: '/api/token',
-    method: 'GET',
-    type: 'url',
-    params: {}
-  },
-  // 获取单个产品的详细数据
-  fetchSingleProductData: {
-    url: '/v1/product-center/product-detail',
-    method: 'GET',
-    type: 'url',
-    params: {}
-  },
-  // 利益演示表
-  fetchInterestData: {
-    url: '/v1/proposal/create',
+  // 在线投保基础资料接口
+  fetchBasicData: {
+    url: '/api/api/invoke',
     method: 'POST',
-    type: 'json',
-    params: {}
-  },
-  //团队信息
-  getTeamInfo: {
-    url: '/v1/membership/team/info',
-    method: 'GET',
-    TYPE: 'json',
-    params: {}
-  },
-  //团队成员列表
-  getTeamMembers: {
-    url: '/v1/membership/team/members',
-    method: 'GET',
-    params: {}
-  },
-  //团队考勤
-  getTeamAttends: {
-    url: '/v1/membership/team/attends',
-    method: 'GET',
-    params: {}
-  },
-  //团队计划书
-  getTeamSchedule: {
-    url: '/v1/membership/team/plans',
-    method: 'get',
-    params: {}
-  },
-  //团队业绩
-  getTeamScores: {
-    url: '/v1/membership/team/scores',
-    method: 'GET',
-    params: {}
-  },
-  //注册/修改密码 验证码接口
-  getRegisterCode: {
-    url: '/v1/membership/agent/sms',
-    method: 'POST',
-    type: 'form',
-    params: {}
-  },
-  //确认提交修改密码
-  confirmPassWord: {
-    url: '/v1/membership/agent/passwd',
-    method: 'POST',
-    type: 'form',
-    params: {}
-  },
-  //确认提交修改密码
-  register: {
-    url: '/v1/membership/agent/register',
-    method: 'POST',
-    type: 'form',
-    params: {}
+    type: 'mix',
+    params: {
+      view: 'true',
+      server: 'PolicyIns.initPolicyField',
+      device: 'mobile'
+    },
+    extra: {
+    }
   }
 }
 
--- test/apitest.js
+++ test/apitest.js
@@ -1,12 +1,9 @@
 const apiConfig = require('./api.config.mjs')
 const CommonApi = require('../index')
 
-let instance = new CommonApi(apiConfig, 'https://ts-api.luckyins.com')
+let instance = new CommonApi(apiConfig, 'https://ts-www.luckyins.com/')
 
-instance.homeBanner({}, {
-  channel: 'channel_test',
-  info_id: '37'
-}).then(data => {
+instance.fetchBasicData({}, {}).then(data => {
   console.log('==> 接口正常返回数据：', data)
 }).catch(error => {
   console.log('==> 接口返回错误数据：', error)
